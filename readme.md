95830e2905b0ee75f78f500a6d444fe88cea3c6e
ngorseck <ngorsecka@gmail.com>
2023-01-20 17:35:35 +0000

    error pages created

---
+++ readme.md
@@ -0,0 +1,3 @@
+#sources : https://www.baeldung.com/spring-boot-keycloak
+https://www.baeldung.com/spring-security-thymeleaf
+https://www.thymeleaf.org/doc/articles/springsecurity.html
--- src/main/java/sn/samane/controller/EmployeeControllerWeb.java
+++ src/main/java/sn/samane/controller/EmployeeControllerWeb.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import sn.samane.domain.Employee;
 import sn.samane.service.EmployeeService;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
@@ -25,7 +27,7 @@ public class EmployeeControllerWeb {
     private EmployeeService employeeService;
 
     //Documentation https://www.baeldung.com/spring-thymeleaf-pagination
-    @PostAuthorize("hasAuthority('ROLE_ADMIN')")
+    @PostAuthorize("hasAuthority('ROLE_SUPER_ADMIN')")
     @GetMapping("/allemployees")
     public String employees (Model model,
                              @RequestParam("page") Optional<Integer> page,
@@ -56,6 +58,24 @@ public class EmployeeControllerWeb {
     @GetMapping("/accueil")
     public String accueil () {
         return "accueil";
+    }
+    @GetMapping("/error")
+    public String error (HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            Integer statusCode = Integer.valueOf(status.toString());
+
+            if(statusCode == HttpStatus.FORBIDDEN.value()) {
+                return "error/403";
+            }else if(statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "error/404";
+            }
+            else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "error/500";
+            }
+        }
+        return "error";
     }
 
     @GetMapping("/logout")
